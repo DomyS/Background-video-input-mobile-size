b5cbfdb1ef2a573f71e893d28f13306876a605d3
Your Name <you@example.com>
2019-09-17 10:58:21 +0200

    Updates

---
+++ precache-manifest.8a7165775ea21fc30c8ac2e005e8dfbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408c80a85c4094c9ce5733ee903fdf61",
+    "url": "/Background-video-input-mobile-size/index.html"
+  },
+  {
+    "revision": "4bf9537f9e0bc153133f",
+    "url": "/Background-video-input-mobile-size/static/css/main.ab93a38f.chunk.css"
+  },
+  {
+    "revision": "0982cfa46c072acd1917",
+    "url": "/Background-video-input-mobile-size/static/js/2.75141d84.chunk.js"
+  },
+  {
+    "revision": "4bf9537f9e0bc153133f",
+    "url": "/Background-video-input-mobile-size/static/js/main.7b95731b.chunk.js"
+  },
+  {
+    "revision": "c1176c6c73ffe7b5f81e",
+    "url": "/Background-video-input-mobile-size/static/js/runtime~main.9d73ab13.js"
+  }
+]);
